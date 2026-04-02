65145fa654b5ad450e480111fb31efdaaeb097e9
KauanDev03 <kauan.cavalcante07@gmail.com>
2026-04-02 19:50:34 -0300

    Add files via upload

---
+++ Exercicio2.java
@@ -0,0 +1,9 @@
+public class Exercicio2{
+    public static void main(String [] args){
+    int num = 15;
+
+        for(int i = 100; num < i; num++)
+            System.out.println("o quadrado de " + num + " é:  " + num * num);
+
+    }
+}
