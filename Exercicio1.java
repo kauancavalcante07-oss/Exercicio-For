7403c01e0f807fd51099a272957702a25adaeb6e
KauanDev03 <kauan.cavalcante07@gmail.com>
2026-04-02 19:46:22 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1{
+    public static void main(String [] args){
+    int num = 100;
+
+        for(int i = 0; num > i; num--)
+            System.out.println(num);
+
+    }
+}
